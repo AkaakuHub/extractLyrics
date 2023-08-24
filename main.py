b6b0eddbbdce20712afdec9d0c0e74422d797d75
あかあく <105225777+AkaakuHub@users.noreply.github.com>
2023-08-24 19:39:14 +0900

    Update main.py
    uta-net, j-lyrics, kkboxに対応

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import sys
 from bs4 import BeautifulSoup
 
 with open("input.txt", "r", encoding="utf-8") as i0:
-    i1 = i0.read().replace("</span>", "\n")
+    l = i0.read()
+
+if '<div jsname="WbKHeb">' in l:
+    # google
+    i1 = l.replace("</span>", "\n")
+else:
+    # uta-net, j-lyrics, kkboxは対応
+    i1 = l.replace("<br>", "\n")
+
 s = BeautifulSoup(i1, "html.parser")
 o1 = s.get_text()
 with open("output.txt", "w") as f:
